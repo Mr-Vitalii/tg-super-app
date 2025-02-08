96b2de8cb10a1260a36881f80f6c4b7f92bae2ff
Vitalii <co4ckowoi.v@gmail.com>
2025-02-08 10:45:35 +0200

    changed the data sending

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,30 +1,57 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import styles from "./Form.module.scss";
 import { useTelegram } from "../../hooks/useTelegram";
 
-import * as apiClient from "../../api-clients";
-import { useMutation } from "react-query";
+/* import * as apiClient from "../../api-clients";
+import { useMutation } from "react-query"; */
 
 export const Form = () => {
   const { tg, user } = useTelegram();
 
   const [userData, setUserData] = useState({});
 
-    const mutation = useMutation(apiClient.register, {
+/*     const mutation = useMutation(apiClient.register, {
     onSuccess: (data) => {
       console.log("Auth success:", data);
-     /*  localStorage.setItem("token", data.token); */
+      localStorage.setItem("token", data.token);
     },
     onError: (error: Error) => {
       console.error("Auth error:", error.message);
     },
-  });
+    }); */
+  
+  
+  const onSendData = useCallback(() => {
+
+    if (user) {
+
+        setUserData({ username: user.username, id: user.id });
+        console.log({ username: user.username, id: user.id });
+        console.log(userData);
+
+      const data = {
+          id: user.id,
+          username: user.username,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          auth_date: tg.initDataUnsafe.auth_date,
+          hash: tg.initDataUnsafe.hash,
+      };
+
+      tg.sendData(JSON.stringify(data));
+    }
+  
+    
+  }, []);
 
   useEffect(() => {
     tg.ready();
     tg.MainButton.setText("Войти").show();
 
-    tg.onEvent("mainButtonClicked", () => {
+
+    tg.onEvent("mainButtonClicked", onSendData);
+
+/*     tg.onEvent("mainButtonClicked", () => {
       if (user) {
         setUserData({ username: user.username, id: user.id });
 
@@ -40,12 +67,12 @@ export const Form = () => {
             hash: tg.initDataUnsafe.hash,
             });
       }
-    });
+    }); */
 
     return () => {
       tg.offEvent("mainButtonClicked");
     };
-  }, []);
+  }, [onSendData]);
 
   return (
     <div className={styles.auth_container}>
