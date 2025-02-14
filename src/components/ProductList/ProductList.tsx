01ec23574b6ae6ecb244f749b84214eb94ded684
Vitalii <co4ckowoi.v@gmail.com>
2025-02-14 16:19:39 +0200

    fix error

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import { useTelegram } from "@/hooks/useTelegram";
 import { Product } from "@/common/types/product";
 import { ProductItem } from "../ProductItem/ProductItem";
-import { Button } from "../Button/Button";
+
 
 
 
