7652fc0bc8e5b6c192e57601aeb99ad19f9e4227
Vitalii <co4ckowoi.v@gmail.com>
2025-08-29 09:27:18 +0300

    add social

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,21 +1,27 @@
 import styles from './Footer.module.scss'
-import { FaFacebook } from 'react-icons/fa'
+import { FaVk } from 'react-icons/fa6'
 import { RiTwitterXFill } from 'react-icons/ri'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaTelegram } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
 
 export const Footer = () => {
   return (
     <div className={styles.footer}>
       <ul className={styles.social}>
         <li className={styles.social__link}>
-          <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
-            <FaFacebook size={35} className={styles.icon} />
+          <a href='https://vk.com/' target='_blank' rel='noreferrer'>
+            <FaVk size={35} className={styles.icon} />
           </a>
         </li>
         <li className={styles.social__link}>
           <a href='https://twitter.com/' target='_blank' rel='noreferrer'>
             <RiTwitterXFill size={35} className={styles.icon} />
+          </a>
+        </li>
+        <li className={styles.social__link}>
+          <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
+            <FaInstagram size={35} className={styles.icon} />
           </a>
         </li>
         <li className={styles.social__link}>
