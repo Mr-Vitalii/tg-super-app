4cd201c9ce85180cd271c37328cee795a5782a04
Vitalii <co4ckowoi.v@gmail.com>
2025-09-16 23:07:23 +0300

    remove social links

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,9 +1,6 @@
 import styles from './Footer.module.scss'
 import { FaVk } from 'react-icons/fa6'
-import { RiTwitterXFill } from 'react-icons/ri'
-import { FaLinkedin } from 'react-icons/fa'
 import { FaTelegram } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
 
 export const Footer = () => {
   return (
@@ -12,21 +9,6 @@ export const Footer = () => {
         <li className={styles.social__link}>
           <a href='https://vk.com/' target='_blank' rel='noreferrer'>
             <FaVk size={35} className={styles.icon} />
-          </a>
-        </li>
-        <li className={styles.social__link}>
-          <a href='https://twitter.com/' target='_blank' rel='noreferrer'>
-            <RiTwitterXFill size={35} className={styles.icon} />
-          </a>
-        </li>
-        <li className={styles.social__link}>
-          <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
-            <FaInstagram size={35} className={styles.icon} />
-          </a>
-        </li>
-        <li className={styles.social__link}>
-          <a href='https://www.linkedin.com/' target='_blank' rel='noreferrer'>
-            <FaLinkedin size={35} className={styles.icon} />
           </a>
         </li>
         <li className={styles.social__link}>
